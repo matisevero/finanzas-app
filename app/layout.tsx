3931f7bc5c566cb8e81ba889c597ef2ecdcf179b
matisevero <108367363+matisevero@users.noreply.github.com>
2026-05-29 15:01:27 -0300

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Roboto, Roboto_Mono } from 'next/font/google'
 import './globals.css'
 
-const roboto     = Roboto({ subsets: ['latin'], variable: '--font-syne', weight: ['400','500','600','700'] })
+const roboto     = Roboto({ subsets: ['latin'], variable: '--font-syne', weight: ['400','500','700'] })
 const robotoMono = Roboto_Mono({ subsets: ['latin'], variable: '--font-space-mono', weight: ['400','700'] })
 
 export const metadata: Metadata = {
